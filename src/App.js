6bb6be32e38f03223e62a62390bd1df95a15dcd7
Felipe Gama Danta <epilefgama2@gmail.com>
2022-02-24 15:19:48 -0300

    ajustando o Lint dos requisitos 1 e 2 com Proptypes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      description: '',
-      attr1: 0,
-      attr2: 0,
-      attr3: 0,
-      image: '',
-      rarity: '',
-      trunfo: true,
-      isSavedButtonDisabled: true,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   // this.state = {
+  //   //   name: '',
+  //   //   description: '',
+  //   //   attr1: 0,
+  //   //   attr2: 0,
+  //   //   attr3: 0,
+  //   //   image: '',
+  //   //   rarity: '',
+  //   //   trunfo: true,
+  //   //   isSavedButtonDisabled: true,
+  //   // };
+  // }
 
   onInputChange = ({ target }) => {
     const { name } = target;
