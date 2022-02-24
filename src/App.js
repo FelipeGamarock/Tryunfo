1b9e4f2d7b947a0ef16506b318c9cd80d428211e
Felipe Gama Danta <epilefgama2@gmail.com>
2022-02-24 16:01:35 -0300

    requisito 4, preview da carta e renderizado na tela

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  //   // this.state = {
-  //   //   name: '',
-  //   //   description: '',
-  //   //   attr1: 0,
-  //   //   attr2: 0,
-  //   //   attr3: 0,
-  //   //   image: '',
-  //   //   rarity: '',
-  //   //   trunfo: true,
-  //   //   isSavedButtonDisabled: true,
-  //   // };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      attr1: 0,
+      attr2: 0,
+      attr3: 0,
+      image: '',
+      rarity: 'normal',
+      trunfo: false,
+      // isSavedButtonDisabled: true,
+    };
+  }
 
   onInputChange = ({ target }) => {
     const { name } = target;
@@ -27,11 +27,41 @@ class App extends React.Component {
   }
 
   render() {
+    const {
+      name,
+      description,
+      attr1,
+      attr2,
+      attr3,
+      image,
+      rarity,
+      trunfo,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form onInputChange={ this.onInputChange } />
-        <Card />
+        <Form
+          onInputChange={ this.onInputChange }
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rarity }
+          cardTrunfo={ trunfo }
+        />
+        <Card
+          onInputChange={ this.onInputChange }
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rarity }
+          cardTrunfo={ trunfo }
+        />
       </div>
     );
   }
